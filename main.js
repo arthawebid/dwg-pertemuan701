73998e9ecd69fc32520ce7f313d9bfd7054771e5
artha.web.id <artha.web.id@gmail.com>
2024-11-14 01:39:28 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Add JS here
+var slideIndex = 1
+    slideshow()
+
+function slideshow(){
+      var i
+      var sd = document.getElementsByClassName("slide")
+      console.log("JumlahSlide: "+ sd.length)
+      for(i=0; i<sd.length; i++){
+        sd[i].style.display = "none"
+      }
+      if(slideIndex>sd.length){
+        slideIndex = 1
+      }
+      sd[slideIndex-1].style.display="block"
+      slideIndex++
+      setTimeout(slideshow,2500)
+}
